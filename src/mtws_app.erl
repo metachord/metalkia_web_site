e99e5e040d5cae88c086d7df2e8a7e79fa4fdfae
Maxim Treskin <zerthurd@gmail.com>
2011-10-18 13:40:37 +0700

    Add stub for fast startup

--- src/mtws_app.erl
+++ src/mtws_app.erl
@@ -3,7 +3,11 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([
+  start/0,
+  start/2,
+  stop/1
+]).
 
 %% ===================================================================
 %% Application callbacks
@@ -12,6 +16,11 @@
 start(_StartType, _StartArgs) ->
   nitrogen_sup:start_link(),
   mtws_sup:start_link().
+
+%% TODO: remove this
+start() ->
+  [application:start(A) || A <- [gproc, riakc, metalkia_riak, metalkia_web_site, exmpp, metalkia_xmpp]].
+
 
 stop(_State) ->
     ok.
