a012c9c649f0ed375dc35d6e611b465c806a06a1
Maxim Treskin <zerthurd@gmail.com>
2011-11-09 18:41:38 +0700

    Edit profile

--- src/mt_profile.erl
+++ src/mt_profile.erl
@@ -6,7 +6,14 @@
   title/0,
   body/0,
   author/0,
-  url/0
+  url/0,
+  event/1
+]).
+
+%% For template
+-export([
+  form_label/1,
+  form_entry/1
 ]).
 
 -include_lib("nitrogen_core/include/wf.hrl").
@@ -36,15 +43,62 @@ body() ->
   ?DBG("PathInfo: ~p", [PathInfo]),
   ?DBG("User: ~p", [wf:user()]),
   Email = mtws_common:get_email(),
-  ?DBG("Email: ~p", [Email]),
   if
     Email =:= undefined ->
       wf:redirect(mtws_common:blog_link());
     true ->
       [
-        #gravatar{email = mtws_common:get_email(), rating = "g"},
+        #gravatar{email = Email, rating = "g"},
         #p{},
-        #label{text = "Name"},
-        #inplace_textbox{text = mtws_common:username()}
+        row(username),
+        row(email),
+        row(name)
       ]
   end.
+
+row(Tag) ->
+  #panel{class = "clearfix", body = [
+    #template{file = "./site/templates/metalkia/form_row.tpl", bindings = [{'Tag', Tag}]}
+  ]}.
+
+
+form_label(Tag) ->
+  Text =
+  case Tag of
+    username -> "Metalkia Username";
+    email -> "Email";
+    name -> "Name";
+    _ -> "-undefined-"
+  end,
+  #label{text = Text}.
+
+form_entry(Tag) ->
+  case Tag of
+    username ->
+      username_entry("", "");
+    email ->
+      Email = mtws_common:get_email(),
+      #textbox{text = Email};
+    name ->
+      Name = mtws_common:username(),
+      #textbox{text = Name};
+    _ -> ""
+  end.
+
+event("check-username") ->
+  UserName = wf:q("input-username"),
+  ?DBG("Check UserName: ~p", [UserName]),
+  case mtc_entry:sget(mt_person, list_to_binary(UserName)) of
+    #mt_person{} ->
+      wf:replace("entry-username", username_entry(UserName, "warning"));
+    _ ->
+      wf:replace("entry-username", username_entry(UserName, "approved"))
+  end.
+
+
+%%
+username_entry(UserName, Class) ->
+  #panel{id = "entry-username", body = [
+    #textbox{id = "input-username", class = "mt-textbox " ++ Class, text = UserName},
+    #button{text = "Check", postback = "check-username"}
+  ]}.
